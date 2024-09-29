ef6bdfbf538eb0314f23c617424f3974cee4fe89
NachoBerridy <nachoberridy@outlook.com>
2024-09-28 21:25:57 -0300

    feat(client): style modifications and componets integrations

--- app/components/organisms/Main.tsx
+++ app/components/organisms/Main.tsx
@@ -2,7 +2,7 @@ import UrlForm from "../molecules/UrlForm";
 
 export default function Main() {
   return (
-    <main className="flex flex-col gap-4  h-screen items-start justify-center">
+    <main className="flex flex-col gap-8  items-start justify-start mt-10 w-full max-w-[1020px]">
       <h1 className="text-4xl font-bold text-center">URL Shortener</h1>
       <UrlForm
         placeholder="Enter a URL"
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
+import Main from "../components/organisms/Main";
+import Header from "../components/organisms/Header";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,8 +11,14 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="flex h-screen items-center justify-center">
-
-    </div >
+    <body className="
+        flex h-screen items-center justify-start w-screen flex-col dark:text-white text-black
+        z-[-2] dark:bg-[#000000] dark:bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] dark:bg-[size:20px_20px]
+        bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] bg-slate-100
+        
+      ">
+      <Header src={null} />
+      <Main />
+    </body >
   );
 }
