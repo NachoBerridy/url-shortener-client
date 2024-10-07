a9a4cff5066602ba6292f70153ea06298026b5a4
NachoBerridy <nachoberridy@outlook.com>
2024-10-07 15:07:43 -0300

    feat(client) app icon

--- app/components/molecules/Navbar.tsx
+++ app/components/molecules/Navbar.tsx
@@ -6,6 +6,7 @@ import { useState, useEffect } from "react";
 import { Link } from "@remix-run/react";
 import { User } from "../../interfaces/user";
 import ProfileNav from "./profileNav";
+import urlIcon from "../../assets/icons/url-icon.svg";
 
 interface NavBarProps {
   src: string | null;
@@ -30,11 +31,10 @@ export default function NavBar({ src, user }: NavBarProps) {
       bg-clip-padding backdrop-blur-sm bg-opacity-10 bg-gray-800
       rounded-3xl shadow-lg z-10
     ">
-      <div className="flex items-center flex-shrink-0 mr-6">
-        <Link className="font-semibold text-xl tracking-tight" to="/">
-          Shortener App
-        </Link>
-      </div>
+      <Link className="flex items-center justify-start flex-shrink-0 mr-6 font-semibold text-2xl tracking-tight" to="/">
+        <img src={urlIcon} alt="url-icon" className="w-8 h-8 inline-block mr-2" />
+        Shortener App
+      </Link>
       <div className="flex items-center">
         {
           theme ? <IconButton icon={<SunIcon />} onClick={() => setTheme(false)} />
